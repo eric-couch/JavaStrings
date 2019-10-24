72ec63f7c388bb68258f50d59e6e27d043e21126
eric-couch <James.Couch@edgetechacademy.edu>
2019-10-24 18:31:46 -0500

    added string methods

--- src/LearningStrings.java
+++ src/LearningStrings.java
@@ -4,17 +4,34 @@
 
 public class LearningStrings {
     public static void main(String[] args) {
-        String name = "James Eric Couch";
-        System.out.println(name.indexOf("x"));
-        // false
-        System.out.println(name.indexOf("C"));
-        // 5
-
-        
+        String name;
+        name = "James Eric Couch";
+//        System.out.println(name.indexOf("x"));
+//        // false
+//        System.out.println(name.indexOf("C"));
+//        // 5
+//        String newName;
+//        newName = name.replace(" ", "-");
+//        System.out.println(newName);
+        // James-Eric-Couch
 
         String[] names = name.split(" ");
-        System.out.println(names[0]);
+        for (int x = 0; x< names.length; x++) {
+            System.out.print(names[x].substring(0, 1) + ". ");
+        }
+        // James
 
+//        String badString = "  Eric Couch  ";
+//        System.out.println(badString.trim());
+//
+//        System.out.println(name.toUpperCase());
+//        // JAMES ERIC COUCH
+//
+//        System.out.println(name.substring(6, 10));
+//
+//        String playIt = "Play it, Sam. Play 'As Time Goes By.'";
+//        String quote = playIt.substring (19, 35);
+//        System.out.println(quote.length());
 
     }
 }
