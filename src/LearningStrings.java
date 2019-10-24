03f5f416f74d3b85cb9e47a02ca314a3b75de917
eric-couch <James.Couch@edgetechacademy.edu>
2019-10-23 19:44:46 -0500

    initial code

---
+++ src/LearningStrings.java
@@ -0,0 +1,9 @@
+//git add -A stages all changes
+//git add . stages new files and modifications, without deletions
+//git add -u stages modifications and deletions, without new files
+
+public class LearningStrings {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
